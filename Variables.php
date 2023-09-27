376401f0837b11cc410ae0af1dc5a4a90befda01
M307users <m307user@m307.uc-bcf.edu.ph>
2023-09-27 13:34:28 +0800

    first commit

---
+++ Variables.php
@@ -0,0 +1,42 @@
+<?php
+
+//integers
+    $int_var = 12345;
+    print "$int_var";
+    $another_int = -12345 + 12345;
+    print "$another_int";
+    echo "<br>";
+
+// doubles
+    $many = 2.2888800;
+    $many_2 = 2.2111200;
+    $few = $many + $many_2;
+    print "($many + $many_2 = $few)";
+    echo "<br>";
+
+
+// boolean
+    $height = 50;
+    $width =25;
+    if ($height == $width) 
+    { 
+    echo "The object is a square";
+    }
+    else 
+    { 
+    echo "The object is not a square<br>";
+    }
+    
+
+// strings
+    $string1 = "Web Information System";
+    $string2 = "Tutorials";
+    $string3 = "Welcome to";
+    echo " $string3  $string1 $string2";
+
+
+// Local Variables
+
+
+       
+?>
