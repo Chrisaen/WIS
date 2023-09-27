99e2493bec6549980e9d00a6fdd1104ff291dab3
M307users <m307user@m307.uc-bcf.edu.ph>
2023-09-27 13:57:53 +0800

    add

--- Variables.php
+++ Variables.php
@@ -32,11 +32,59 @@
     $string1 = "Web Information System";
     $string2 = "Tutorials";
     $string3 = "Welcome to";
-    echo " $string3  $string1 $string2";
+    echo " $string3  $string1 $string2 <br>";
 
 
 // Local Variables
+    $x = "Addition";
+    function assignx () {
+    $x = "Multiplication";
+    print "\$x inside function is $x. <br> ";
+    }
+    assignx();
+    print "\$x outside of function is $x.<br> ";
 
+
+// Function Parameters
+
+// multiply a value by 10 and return it to the caller
+function addition ($value) {
+    $value = $value + 10;
+    return $value;
+   }
+   $retval = addition (10);
+   Print "Return value is $retval\n <br>";
+
+// Global Variables
+
+$a = 1;
+$b = 2;
+
+function Sum()
+{
+    global $a, $b;
+
+    $b = $a + $b;
+} 
+
+Sum();
+echo $b;
+echo "<br>"
+
+
+// Static Variables
+function static_var()  
+{  
+    // static variable  
+    static $num = 5;  
+    $total = 2;  
+  
+    $sum++;  
+    $num++;  
+  
+    echo $num, "\n";  
+    echo $sum, "\n";  
+}  
 
        
 ?>
